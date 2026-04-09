35766d60b1ce8b1374a33a45664a594e60a4568c
Mayank Singh <mayankrajput5455@gmail.com>
2026-04-09 09:04:19 +0530

    Uploading all files

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load('KNN_heart.pkl')
+scaler = joblib.load('scaler.pkl')
+expected_col = joblib.load('columns.pkl')
+
+st.title("Heart Stroke Prediction By Mayank ❤️")
+st.markdown("Provide the following details")
+
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex", ['M', 'F'])
+chest_pain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+resting_bp = st.number_input("Resting Blood Pressure (in mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Cholesterol (mg/dL)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0, 1])
+resting_egc = st.selectbox("Resting ECG", ['Normal', 'ST', "LVH"])
+max_hr = st.slider("Maximum Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ["Y", "N"])
+oldpeak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0)
+st_slope = st.selectbox("ST slope", ["Up", "Flat", "Down"])
+
+if st.button("Predict"):
+
+    raw_input = {
+        'Age': age,
+        'RestingBP': resting_bp,
+        'Cholesterol': cholesterol,
+        'FastingBS': fasting_bs,
+        'MaxHR': max_hr,
+        'Sex_' + sex: 1,
+        'ChestPainType_' + chest_pain: 1,
+        'RestingECG_' + resting_egc: 1,
+        'ExerciseAngina_' + exercise_angina: 1,
+        'ST_Slope_' + st_slope: 1
+    }
+
+    input_df = pd.DataFrame([raw_input])
+
+    for col in expected_col:
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    input_df = input_df[expected_col]
+
+    scaled_input = scaler.transform(input_df)
+
+    prediction = model.predict(scaled_input)[0]
+
+    if prediction == 1:
+        st.error("⚠️ High Risk of Heart Disease")
+    else:
+        st.success("✅ Low Risk of Heart Disease")
